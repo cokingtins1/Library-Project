1b1ee7aa2d73306822e4d2fb0a4f27ea6b869105
cokingtins1 <106932061+cokingtins1@users.noreply.github.com>
2023-07-08 13:21:52 +0000

    final commit of finished proejct

--- script.js
+++ script.js
@@ -4,17 +4,14 @@ let myLibrary = []
 const addBook = document.getElementById("newBook")
 const submit = document.getElementById("submit")
 
-
 var popUp = document.getElementById('myModal')
 var span = document.getElementsByClassName("close")[0];
-
-
-var out1 = document.getElementById('output1')
 
 const titleInput = document.getElementById('title')
 const authorInput = document.getElementById('author')
 const pagesInput = document.getElementById('pages')
 
+// constructor for Book
 class Book{
     constructor(title, author, pages){
         this.title = title
@@ -23,93 +20,84 @@ class Book{
     }
 }
 
+// display pop-up for input fields, clear previous inputs from localstorage
 addBook.addEventListener('click', () => {
     popUp.style.display = "block"
+    titleInput.value = ""
+    authorInput.value = ""
+    pagesInput.value = ""
+
 })
 
 let newBook
 function addBookToLibrary(){
     newBook = new Book(titleInput.value, authorInput.value, pagesInput.value)
-    // myLibrary.push(JSON.stringify(newBook))
     myLibrary.push(newBook)
-    // out1.innerHTML = myLibrary[0].title
-    console.log(newBook.title)
     popUp.style.display = "none"
+    restore()
+}
+
+function render(){
+    const display = document.getElementById('Library-container')
+    const books = document.querySelectorAll('.book')
+    books.forEach(book => display.removeChild(book))
 
     for(let i =0; i<myLibrary.length; i++){
         createBook(myLibrary[i]);
     }
 
+}
+
+// DOM manipulation
+function createBook(item){
     const libcontainer = document.getElementById('Library-container');
     const bookDiv = document.createElement('div')
     const titleDiv = document.createElement('div')
+    const authorDiv = document.createElement('div')
+    const pagesDiv = document.createElement('div')
 
-    bookDiv.classList.add('Book')
+    bookDiv.classList.add('book')
     libcontainer.append(bookDiv)
     
     titleDiv.classList.add('title')
-    titleDiv.innerText = myLibrary[0].title
+    titleDiv.innerText = "Title: " + item.title
     bookDiv.append(titleDiv)
 
+    authorDiv.classList.add('author')
+    authorDiv.innerText = "Author: " + item.author
+    bookDiv.append(authorDiv)
 
+    pagesDiv.classList.add('pages')
+    pagesDiv.innerText = "Pages: " + item.pages
+    bookDiv.append(pagesDiv)
 }
 
-function createBook(item){
-    // const library = document.querySelector('#Library-container');
-    const libcontainer = document.getElementById('Library-container');
-    const library = document.createElement('div')
-    const bookDiv = document.createElement('div');
-
-    libcontainer.append(library)
-    // library.classList.add('Library-container')
-    // bookDiv.classList.add('book')
-    // bookDiv.setAttribute('id', myLibrary.indexOf(item))
-
-    
-
+// stores myLibrary to local storage
+function storeData(){
+    localStorage.setItem(`myLibrary`, JSON.stringify(myLibrary))
 }
 
+
+// checks is local storage contains data, gets data if true, renders data if false
+function restore(){
+    if(!localStorage.myLibrary){
+        render()
+    }else{
+        let objects = localStorage.getItem('myLibrary')
+        objects.JSON.parse(objects)
+        myLibrary = objects
+        render()
+        console.log(myLibrary)
+    }
+}
+
+// "submit" button functionality
 submit.addEventListener('click', addBookToLibrary)
 
 
+// "x" button functionality
 span.onclick = function() {
     popUp.style.display = "none";
   }
 
-// function Player(name, marker){
-//     this.name = name
-//     this.marker = marker
-//     this.sayName = function() {
-//         console.log("hello" + name)
-//     }
-// }
 
-// const player1 = new Player("sean", "x")
-
-
-// function Book(title, author, pages){
-//     this.title = title
-//     this.author = author
-//     this.pages = pages
-
-//     this.info = function() {
-//         // return (this.title + " by " + ${this.author} + " has " + this.pages + " pages.")
-
-//         return (`${this.title} is a book`)
-//     }
-// }
-
-// const book1 = new Book("The Hobbit", "J.R.R Tolkien", "295")
-
-// console.log(book1.info())
-
-
-
-// for (var i = 0; i < newBook.length; i++) {
-//     var button = newBook[i]
-//     newBook.addEventListener('click', function (e) {
-//         console.log('clicked')
-
-//     });
-
-// }
